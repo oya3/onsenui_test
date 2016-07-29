8b2fe13d7d55d54c34d1416edd25ec350007276c
Kazunori OYA <terje@oya3.net>
2016-07-29 10:43:31 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+// gulpfile.js
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var webserver = require('gulp-webserver');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+ 
+var paths = {
+  scripts: ['app/js/**/*.coffee', '!app/external/**/*.coffee']
+};
+ 
+// Webサーバの機能です
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+    }));
+});
+ 
+// スクリプトの結合と配置を行っています
+gulp.task('scripts', function() {
+  return gulp.src([
+    './bower_components/angular/angular.min.js',
+    './bower_components/onsenui/js/onsenui_all.min.js'
+  ])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./javascripts/'));
+});
+ 
+// ファイルの変更を監視しています（今回は使っていません）
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['scripts']);
+});
+ 
+gulp.task('default', ['webserver', 'scripts', 'watch']);
